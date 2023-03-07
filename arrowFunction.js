43d8a73d0b6a6049202a3ebc6c915e2749434951
SnSDk <sergiosnsdk@gmail.com>
2023-03-07 18:03:06 -0300

    arrow function tutorial

---
+++ arrowFunction.js
@@ -0,0 +1,28 @@
+let suma = function(a,b){
+    return a + b
+}
+
+
+let sumaFlecha = (a,b) => a + b
+
+
+const nombreFuncion = (parametro1,parametro2,parametroetc) => {
+    declaracion(1)
+    declaracion(2)
+    declaracion(etc)
+}
+
+
+
+const funcion = param => retorno
+
+
+
+const preguntar = (pregunta, si, no) => (confirm(pregunta)) ? si() : no ();
+
+preguntar(
+    '¿Desea seguir aprendiendo Javascript?',
+    () => alert('Siga a @sergiecode y bienvenido al mundo Javascript'),
+    () => alert('¡Qué pena! Siga a @sergiecode que también abarca otros lenguajes')
+)
+
